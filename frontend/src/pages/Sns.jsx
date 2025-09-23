7a2cde8b7fd7743598be2d7e0878f21468682298
yoonsh-ca <yoonsh330@gmail.com>
2025-09-23 02:46:11 -0600

    feat: Add SNS link page

--- frontend/src/pages/Sns.jsx
+++ frontend/src/pages/Sns.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 export default function Sns() {
-  return <div>SNS</div>;
+  return (
+    <div>
+      <Navbar />
+      <h1>K-Town SNS</h1>
+      <p>Follow our SNS account, and receive various news!</p>
+      <div>
+        <a href='https://www.kplaytown.ca'>
+          <span>K-Town</span>
+        </a>
+      </div>
+      <div>
+        <a href='https://www.instagram.com/ktown_edmonton'>
+          <span>Instagram</span>
+        </a>
+      </div>
+      <div>
+        <a href='https://www.tiktok.com/@ktownsgm'>
+          <span>TikTok</span>
+        </a>
+      </div>
+    </div>
+  );
 }
